5392d12d2b8ecb9addca73b7fe2d9c739da73c12
AleksandrZhuravel <57489604+AleksandrZhuravel@users.noreply.github.com>
2020-08-18 16:26:34 +0300

    Сomments have been removed

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -12,55 +12,39 @@ import static com.codeborne.selenide.Selenide.open;
 class GoogleTests {
     @Test
     void selenideSearchTest() {
-        // Открыть google
         open("https://google.com");
-
-        // Ввести Selenide в поиск
+     
         $(byName("q")).setValue("Selenide").pressEnter();
-
-        // Проверить, что Selenide появился в результатах поиска
+        
         $("html").shouldHave(text("ru.selenide.org"));
     }
 
     @Test
     void picturePageShouldOpenTest() {
-        // Открыть google
         open("https://google.com");
 
-        // Нажать вкладку "Картинки"
-        // $("[data-pid='2']").click(); //обращение по кастомному атрибуту
         $(by("data-pid", "2")).click();
         $(byText("Картинки")).click();
 
-        // Проверить наличие заголовка "Картинки Google"
-        // $("[title='Картинки Google']").shouldBe(visible); //обращение по атрибуту
         $(byTitle("Картинки Google")).shouldBe(visible);
     }
 
     @Test
     void googleApplicationsPopupShouldBeExpandedTest() {
-        // Открыть google
         open("https://google.com");
 
-        // Нажать на ссылку выпадающего списка "Приложения Google"
-        $(byTitle("Приложения Google")).click(); //обращение по CSS-селектору, полученному из XPath
-
-        // Проверить видимость выпадающего списка "Приложения Google"
-        $("iframe[role='presentation']").shouldBe(enabled); //обращение по CSS-селектору, полученному из XPath
+        $(byTitle("Приложения Google")).click(); 
+        
+        $("iframe[role='presentation']").shouldBe(enabled); 
     }
 
     @Test
     void yandexUnitFrameShouldOpen() {
-        // Открыть yandex
         open("https://yandex.ru");
 
-        // Нажать на ссылку "Настройка"
-        $(byText("Настройка")).click(); //обращение по содержимому тега a
+        $(byText("Настройка")).click(); 
+        $(byText("Настроить блоки")).click();
 
-        // Нажать пункт "Настроить блоки"
-        $(byText("Настроить блоки")).click(); //обращение по содержимому тега span
-
-        // Проверить открытие окна "Блоки на странице"
-        $(".hidden-blocks-popup__inside").shouldBe(enabled); //обращение по имени класса
+        $(".hidden-blocks-popup__inside").shouldBe(enabled); 
     }
 }
